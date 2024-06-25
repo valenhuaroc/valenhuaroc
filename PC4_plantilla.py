75d98624a28618b36b628149396ad08688371525
valenhuaroc <a20212370@pucp.edu.pe>
2024-06-25 09:59:35 -0500

    Add files via upload

---
+++ PC4_plantilla.py
@@ -0,0 +1,157 @@
+# Este código sirve para instalar Streamlit en tu computadora y hacer un primer programa en Streamlit.
+# Pero se ejecuta en la terminal de tu computadora, no en Jupyter Notebook.
+# pip install Streamlit
+
+# Este código sirve para acceder una página web en tu navegador que te brinda información sobre Streamlit.
+# Pero se ejecuta en la terminal Python de tu computadora, no en Jupyter Notebook.
+# streamlit hello
+
+# Antes de ejecutar un script de Python en Streamlit debes definir la carpeta donde se encuentra tus archivos
+# cd ruta_de_tu_carpeta
+
+# Este comando sirve para ejecutar un script de Python en Streamlit.
+# Pero se ejecuta en la terminal de tu computadora, no en Jupyter Notebook.
+# OJO: Debes antes tener instalado Streamlit en tu computadora, debes antes definir la ruta de tus archivos y 
+##     tener un script de Python (your_script.py) que quieras ejecutar en Streamlit.
+#  streamlit run your_script.py
+
+# Este código sirve para hacer un primer programa en Streamlit.
+import streamlit as st
+
+# Creamos una barra lateral
+sidebar = st.sidebar
+
+# Si usas el siguiente código mostrará un título en la aplicación Streamlit. 
+# st.title("Nombre de tu blog"): Esta línea está creando un título en la aplicación web.
+# Pero, a diferencia de st.markdown, el texto estará alineado a la izquierda y no podrás cambiar el color del texto.
+
+# La función st.markdown permite centrar y agrandar la letra del título de la web en Streamlit.
+st.markdown("<h1 style='text-align: center;'>AMOR ODIO CON PYTHON ❤️‍🩹</h1>", unsafe_allow_html=True)
+
+# <h1 style='text-align: center;'>Nombre de tu blog</h1>: Esto es una cadena de código HTML. 
+# La etiqueta <h1> se utiliza para el encabezado principal de una página web, y 
+# el atributo style se utiliza para agregar estilos CSS. 
+# En este caso, el texto está alineado al centro (text-align: center;). 
+# El texto dentro de las etiquetas <h1> ("Aquí escribe un nombre creativo para tu blog") es el contenido del encabezado.
+
+# unsafe_allow_html=True: Este es un argumento opcional en la función markdown. 
+# Por defecto, streamlit no permite HTML en el texto de Markdown por razones de seguridad. 
+# Sin embargo, establecer unsafe_allow_html en True permite el uso de HTML.
+
+# Creamos dos columnas separadas para la imagen y el texto
+col1, col2 = st.columns(2)
+
+# col1, col2 = st.columns(2): Esta línea está creando dos columnas en la interfaz de usuario de la aplicación web. 
+# La función st.columns toma un número entero como argumento que especifica el número de columnas que se deben crear. 
+# Las columnas creadas se asignan a las variables col1 y col2.
+
+# En la primera columna colocamos la imagen
+col1.image("valentina.png", caption='Estudiante de periodismo que jamás pensó programar y ahora le parece hasta divertido', width=300)
+
+# col1.image("ellie.png", caption='Ellie', width=300): Esta línea está colocando una imagen en la primera columna (col1). 
+# La función image toma como primer argumento la ruta de la imagen que se va a mostrar. 
+# En este caso, la imagen es "ellie.png". 
+# El argumento caption se utiliza para proporcionar un pie de foto a la imagen, en este caso 'Ellie'. 
+# El argumento width se utiliza para especificar el ancho de la imagen, en este caso 300 píxeles.
+
+# En la segunda columna colocamos el texto: Debe contener una presentación de ustedes
+# Deben presentarse: ¿Quién eres?, ¿De dónde eres?, ¿Qué estudias?, ¿Qué te gusta de tu carrera?, 
+# ¿Qué te gustaría hacer en el futuro?, ¿Qué te gusta hacer en tu tiempo libre?
+
+texto = """
+Hola, me llamo Valentina, tengo 20, me siento de 18. Soy capricornio y medio workalcoholic. Vivo a hora y media de PUCP y a veces tengo que correr para chapar micro. Me gusta el jazz, la poesía, leo el tarot y soy espiritualista. Leo biografías o diarios para entender a autores y mis principales referentes son Idea Vilariño, Pizarnik, Vallejo y Albert Camus. Soy introvertida y me gusta mucho escribir. Adoro las aceitunas verdes, la palta, las mandarinas. Los makis están sobrevalorados pero son ricos y en general, me gusta romantizar el día a día con música. Tengo dos perritas, una gata y quisiera adoptar a un erizo. Nunca me cambiaría de carrera pero sí estudiaría otra. Ojalá me diera la vida para seguir leyendo y/o escribiendo.
+"""
+
+# Las comillas triples (""") en Python se utilizan para definir cadenas multilínea.
+# Mostramos el texto
+col2.markdown(f"<div style='text-align: justify; font-size: 15px;'>{texto}</div>", unsafe_allow_html=True)
+
+# <div style='text-align: justify; font-size: 15px;'>{texto}</div>: Esta es una cadena de código HTML. 
+# La etiqueta <div> se utiliza para agrupar contenido en HTML. 
+# En este caso, el texto está justificado (text-align: justify;). 
+# El tamaño de la fuente se establece en 15 píxeles (font-size: 15px;).
+# El texto dentro de las etiquetas <div> es la variable texto.
+
+# f"": Esta es una cadena de formato en Python. 
+# Permite la interpolación de variables, lo que significa que puedes insertar el valor de una variable directamente en la cadena. 
+# En este caso, {texto} se reemplaza por el valor de la variable texto.
+
+
+# Debajo de las columnas colocamos un subtítulo que describa tu experiencia aprendiendo a programar
+# Deben presentar tu experiencia aprendiendo a programar: ¿Cómo te sentiste al principio?, 
+# ¿Qué te ha enseñado la programación?, ¿Qué te gusta de programar?, 
+# ¿Qué te gustaría hacer con la programación en el futuro? 
+
+# Agregamos un subtítulo
+st.markdown("<h2 style='text-align: center;'>Aprendiendo a programar 💻</h2>", unsafe_allow_html=True)
+
+# <h2 style='text-align: center;'>Mi experiencia aprendiendo a programar 💻</h2>: Esta es una cadena de código HTML.
+# La etiqueta <h2> se utiliza para el encabezado secundario de una página web.
+# El texto está centrado (text-align: center;).
+# El texto dentro de las etiquetas <h2> ("Mi experiencia aprendiendo a programar 💻") es el contenido del encabezado
+# unsafe_allow_html=True: Este es un argumento opcional en la función markdown.
+# Por defecto, streamlit no permite HTML en el texto de Markdown por razones de seguridad.
+# Sin embargo, establecer unsafe_allow_html en True permite el uso de HTML.
+# Puedes agregar emojis en el texto de Markdown utilizando códigos de emoji.
+
+# Agregar un  texto para la respuesta
+texto_2 = """
+Mi experiencia y primer acercamiento a la programación fue bastante gratificante y enriquecedora, diría que hasta desafiante. Jamás me imaginé viendo códigos y batallando frente a un computador con númros y letras que seguían un patrón. Hubo días en los que me estresé bastante y qería recurrir a amigos que sabían mucho más que yo para que me ayudaran pero entendí que la que tenía que aprender era yo. Admito que en un inicio me sentía muy mareada y que me vi muchos videos de Youtube para entender como se utilizaban tales funciones. Programar no es tan difícil, creo que es ahondar más en lo lógico y yo soy más de racionalidad o creativa. En general, en algun momento fue muy agobiante, pero lueo se volvió divertido.
+"""
+
+# Mostramos el texto
+st.markdown(f"<div style='text-align: justify; font-size: 15px;'>{texto_2}</div>", unsafe_allow_html=True)
+
+# <div style='text-align: justify; font-size: 15px;'>{texto_2}</div>: Esta es una cadena de código HTML.
+# La etiqueta <div> se utiliza para agrupar contenido en HTML.
+# En este caso, el texto está justificado (text-align: justify;).
+# El tamaño de la fuente se establece en 15 píxeles (font-size: 15px;).
+# El texto dentro de las etiquetas <div> es la variable texto_2.
+# f"": Esta es una cadena de formato en Python.
+# Permite la interpolación de variables, lo que significa que puedes insertar el valor de una variable directamente en la cadena.
+# En este caso, {texto_2} se reemplaza por el valor de la variable texto_2.
+
+# Agregamos un subtítulo en la barra lateral
+sidebar.markdown("<h1 style='text-align: center;'>Gráficos trabajados en clase</h1>", unsafe_allow_html=True)
+
+# <h1 style='text-align: center;'>Los análisis de Ellie</h1>: Esta es una cadena de código HTML.
+# La etiqueta <h1> se utiliza para el encabezado principal de una página web.
+# El texto está centrado (text-align: center;).
+# El texto dentro de las etiquetas <h1> ("Los análisis de Ellie") es el contenido del encabezado.
+
+# Creamos una lista de gráficos
+graficos = [ 'Barras de númerro de lenguas', 'Gráfico de Familias lingüísticas', 'Gráfico de películas']
+
+# Creamos un cuadro de selección en la barra lateral
+grafico_seleccionado = sidebar.selectbox('Selecciona un gráfico', graficos)
+# El cuadro de selección se crea con la función selectbox.
+# El primer argumento es el texto que se muestra en el cuadro de selección.
+# El segundo argumento es una lista de opciones que se pueden seleccionar.
+# En este caso, las opciones son los elementos de la lista graficos.
+# La opción seleccionada se asigna a la variable grafico_seleccionado.
+# La variable grafico_seleccionado se utiliza para mostrar el gráfico correspondiente en la aplicación web.
+# La función selectbox se utiliza para crear un cuadro de selección en la barra lateral.
+
+# Mostramos el gráfico seleccionado
+if grafico_seleccionado == 'Barras de númerro de lenguas':
+    sidebar.markdown("<div style='text-align: justify; font-size: 20px;'>En este gráfico se visualiza el número de lenguas por familia linguística trabajado en clase.</div>", unsafe_allow_html=True)
+    sidebar.image("barras.png", caption='Barras de númerro de lenguas', width=500)
+    pass
+elif grafico_seleccionado == 'Gráfico de Familias lingüísticas':
+    sidebar.markdown("<div style='text-align: justify'>En este gráfico se visualiza el mapa de la familia linguistica quechua en sudamérica.</div>", unsafe_allow_html=True)
+    sidebar.image("mapa.png", caption='Gráfico de Familias lingüísticas', width=500)
+    pass
+elif grafico_seleccionado == 'Gráfico de películas':
+    sidebar.markdown("<div style='text-align: justify'>Aquí se visualiza el mapa mundial de ubicación de películas que se escogieron para el ejercicio.</div>", unsafe_allow_html=True)
+    sidebar.image("mapa2.png", caption='Gráfico de películas', width=500)
+    pass
+
+# if grafico_seleccionado == 'Gráfico de Macroareas':: Esta línea verifica si la opción seleccionada es 'Gráfico de Macroareas'.
+# Si es así, se ejecuta el código dentro del bloque if.
+# En este caso, se muestra un texto y una imagen correspondientes al gráfico de Macroareas.
+# El texto y la imagen se muestran en la barra lateral.
+# La función markdown se utiliza para mostrar el texto en la barra lateral.
+# La función image se utiliza para mostrar la imagen en la barra lateral.
+# El argumento caption se utiliza para proporcionar un pie de foto a la imagen.
+# El argumento width se utiliza para especificar el ancho de la imagen.
+# La palabra clave pass se utiliza para indicar que no se debe hacer nada en caso de que la opción seleccionada no sea 'Gráfico de Macroareas'.
